cde5484ac7d7863fae564d7acb4c5da6146f912d
Anmolxx <anmoldeepkaur1103@gmail.com>
2026-04-08 00:40:27 +0530

    fix: allow S3 images in next config

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "staging.ilovememes.com",
-          pathname: "/api/v1/files/**",
-        },
-      ],
-    },
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "staging.ilovememes.com",
+        pathname: "/api/v1/files/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i-love-memes-files.s3.ap-southeast-2.amazonaws.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;

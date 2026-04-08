c41182aa0803423bb4888c38bb3a53d5732dde93
Anmolxx <anmoldeepkaur1103@gmail.com>
2026-04-08 15:35:16 +0530

    fix: tag creation and S3 canvas rendering

--- src/components/memes/SaveTemplateButton.tsx
+++ src/components/memes/SaveTemplateButton.tsx
@@ -169,7 +169,7 @@ const SaveTemplateButton: React.FC<SaveTemplateButtonProps> = ({
                   selectedTags={selectedTags}
                   setSelectedTags={setSelectedTags}
                   variant='dialog'
-                  createTagMutation={createTag} // Use the 'dialog' variant for full width and better display in the dialog
+                // Use the 'dialog' variant for full width and better display in the dialog
                 />
               </FormControl>
             </FormItem>
